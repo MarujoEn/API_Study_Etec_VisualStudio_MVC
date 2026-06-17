5f85a6e6f3ed2a181f3ba12dc1727d308265810b
Maru <marujoprofissional@gmail.com>
2026-06-17 19:47:53 -0300

    feat: Making the model of the POST for the API.

---
+++ API_Study_ETEC/Models/POST.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Study_ETEC.Models
+{
+    public class POST
+    {
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Body { get; set; }
+    }
+}
